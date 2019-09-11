ed12313a01168918c335fc245b9f908d0119c003
ghobekang <dreamty88@gmail.com>
2019-09-11 10:34:11 +0900

    d

--- hook.php
+++ hook.php
@@ -4,18 +4,15 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '847825836:AAFv02ESsTVjnrzIomgdiVjBGWVw7CpN_Cg';
 $bot_username = 'aqoom_bot';
-$hook_url     = 'https://localhost/hook.php';
 
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
-    // Set webhook
-    $result = $telegram->setWebhook($hook_url, ['certificate' => './cert/cert.pem']);
-    if ($result->isOk()) {
-        echo $result->getDescription();
-    }
+    // Handle telegram webhook request
+    $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // Silence is golden!
     // log telegram errors
     // echo $e->getMessage();
 }
